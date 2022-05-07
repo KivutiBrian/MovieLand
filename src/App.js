211bc7844ab1c39cafa620f382d35f0a458b8500
Gaideh <briankivuti1@gmail.com>
2022-05-07 16:19:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import SearchIcon from "./search.svg"
 
-function App() {
+// components
+import MovieCard from "./components/MovieCard"
+
+// API KEY 
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`
+
+const App = () => {
+
+  const [movies, setMovies] = useState([])
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    setMovies(data.Search)
+  }
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+ 
+  // fetch the data on page load
+  useEffect(() => {
+    searchMovies("you")
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      {/* Seatch */}
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+
+        />
+        <img src={SearchIcon} onClick={() => searchMovies(searchTerm)} alt='search' />
+      </div>
+
+      {
+        movies.length > 0 ? (
+          <div className='container'>
+
+            {movies.map(movie => <MovieCard key={movie.imdbID} movie={movie} />)}
+
+          </div>
+        ) : (
+          <div className='empty'>No movies found</div>
+        )
+      }
+
+
+
     </div>
-  );
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Person = (props) => {
+//   return (
+//     <>
+
+//       <h1>First Name:{props.fname} </h1>
+//       <h2>Last Name: {props.lname}</h2>
+//       <h2>Age: { props.age }</h2>
+
+//     </>
+//   )
+// }
+
+// const App = () => {
+
+//   const name = null
+//   const isNameShowing = false
+
+//   const [counter, setCounter] = useState(0)
+
+//   useEffect(()=>{
+//     setCounter(10)
+//   }, [])
+
+
+//   return (
+//     <div className="App">
+//       <h1>Hello {isNameShowing ? name : ''}</h1>
+//       {name ? (
+//         <>Test</>
+//       ) : (
+//         <>
+//           <p>lorem sada</p>
+//           <button>Click me</button>
+//         </>
+//       )}
+
+//       <Person fname={'Gaideh'} lname='Brian' age={10} />
+
+//       <button onClick={()=> setCounter(prevCount => prevCount - 1)}>-</button>
+//       <h1>{counter}</h1>
+//       <button onClick={()=> setCounter(prevCount => prevCount + 1)}>+</button>
+
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MovieCard = ({movie}) => {
+
+
+    return (
+        <div className='movie'>
+            {/* Movie Year */}
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+            {/* Movie Img */}
+            <div>
+                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+            </div>
+            {/* Movie Type */}
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
